1d9bab917e4bd8b413f47dde2067dbea6951139d
mchu-1999 <jerry.ee06@nctu.edu.tw>
2024-06-09 15:13:09 +0800

    5.18: Blog List End To End Testing, step 2

--- blogs-e2e/tests/blogs.spec.js
+++ blogs-e2e/tests/blogs.spec.js
@@ -14,9 +14,26 @@ describe('Blog app', () => {
   })
 
   test('Login form is shown', async ({ page }) => {
-    await page.getByTestId('username').fill('superadmin')
-    await page.getByTestId('password').fill('sudo')
-    await page.getByRole('button', { name: 'Login' }).click() 
-    await expect(page.getByText(/super admin/)).toBeVisible()
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible()
+    await expect(page.getByTestId('username')).toBeVisible()
+    await expect(page.getByTestId('password')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('superadmin')
+      await page.getByTestId('password').fill('sudo')
+      await page.getByRole('button', { name: 'Login' }).click() 
+      
+      await expect(page.getByText(/super admin/)).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('superadmin')
+      await page.getByTestId('password').fill('WRONG-PASSWORD')
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.getByText(/wrong username or password/)).toBeVisible()
+    })
   })
 })
